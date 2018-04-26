e0a37dde6b0b5cc09eda7d66d1e9ba677668ce10
chinaljs <455366198@qq.com>
2018-04-26 11:17:13 +0800

    l

---
+++ src/test/java/com/ljs/demo/DemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.ljs.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	public String name;
+	@Test
+	public void contextLoads() {
+
+	}
+
+}
